029972a40d4b6206e111f924c5c4330eaa15dc5f
Biswarup Bhattacharjee <74323889+biswa2210@users.noreply.github.com>
2021-07-23 21:35:34 +0530

    added by biswa

--- Detection.py
+++ Detection.py
@@ -1,4 +1,9 @@
 # import the necessary packages
+'''
+CREATED BY BISWARUP BHATTACHARJEE
+EMAIL    : bbiswa471@gmail.com
+PHONE NO : 6290272740
+'''
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
